b237c5a04fc2071d2e77f4749e76453497b8bb7d
Terence <35042213+reshinto@users.noreply.github.com>
2022-04-03 20:38:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 # chmod a+x run.sh
 
+# digital ocean cloud environment
+# sudo docker stop newyear
+# sudo docker rm newyear
+# sudo docker rmi newyear
+# sudo docker build -t newyear .
+# sudo docker run -d -p 2211:80 --name newyear --restart unless-stopped newyear
+
+# local environment
 PUBLIC_URL=. yarn run build
-docker stop app
-docker rm app
-docker rmi app
-docker build -t app .
-docker run -d -p 2211:80 --name app --restart unless-stopped app
+docker stop newyear
+docker rm newyear
+docker rmi newyear
+docker build -t newyear .
+docker run -d -p 2211:80 --name newyear --restart unless-stopped newyear
